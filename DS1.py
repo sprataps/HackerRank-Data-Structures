32f63e64083c1947d4a32fb8e56dbed4eba43b1b
Siddharth Pratap Singh <siddharthpratap92@gmail.com>
2017-09-17 21:43:52 -0400

    Arrays

---
+++ DS1.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+
+import sys
+
+
+n = int(input().strip())
+arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+
+for i in range(len(arr)-1,-1,-1):
+    print(arr[i],end=" ")
